f49811e2ad74f757ed81166515e940909d76bbd1
rpjengaard <rpj@limbo.works>
2022-03-28 20:28:12 +0200

    feat: add mobile navigation
    #1

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -10,8 +10,8 @@
           </NuxtLink>
         </div>
         <div class="-mr-2 -my-2 md:hidden">
-          <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-            <span class="sr-only">Open menu</span>
+          <button @click="toggleBurger = !toggleBurger" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+            <span class="sr-only">Åbn menu</span>
             <!-- Heroicon name: outline/menu -->
             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -39,7 +39,7 @@
         From: "opacity-100 scale-100"
         To: "opacity-0 scale-95"
     -->
-    <div class="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden hidden">
+    <div id="burgermenu" v-show="toggleBurger" class="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
       <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
         <div class="pt-5 pb-6 px-5 sm:pb-8">
           <div class="flex items-center justify-between">
@@ -47,8 +47,8 @@
               <img class="h-8 w-auto" src="~/assets/logo.svg" alt="Bevägelse">
             </div>
             <div class="-mr-2">
-              <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                <span class="sr-only">Close menu</span>
+              <button @click="toggleBurger = !toggleBurger" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span class="sr-only">Luk menu</span>
                 <!-- Heroicon name: outline/x -->
                 <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -59,65 +59,54 @@
           <div class="mt-6 sm:mt-8">
             <nav>
               <div class="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gray-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/chart-bar -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                    </svg>
+                <NuxtLink to="/" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                  <div class="ml-4 text-xl text-bevagelse-blue-dark">
+                    Hjem
                   </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">
-                    Hvad er?
-                  </div>
-                </a>
+                </NuxtLink>
 
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gray-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/cursor-click -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                    </svg>
+                <NuxtLink to="/ydelser/" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                  <div class="ml-4 text-xl text-bevagelse-blue-dark">
+                    Ydelser
                   </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">
-                    Referencer
-                  </div>
-                </a>
+                </NuxtLink>
 
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gray-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/shield-check -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg>
+                <NuxtLink to="/om-bevaegelse/" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                  <div class="ml-4 text-xl text-bevagelse-blue-dark">
+                    Om Bevägelse
                   </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">
-                    Video
-                  </div>
-                </a>
+                </NuxtLink>
 
-                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                  <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-gray-500 text-white sm:h-12 sm:w-12">
-                    <!-- Heroicon name: outline/view-grid -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                    </svg>
+                <NuxtLink to="/cases/" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                  <div class="ml-4 text-xl text-bevagelse-blue-dark">
+                    Cases
                   </div>
-                  <div class="ml-4 text-base font-medium text-gray-900">
+                </NuxtLink>
+
+                <NuxtLink to="/kontakt/" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
+                  <div class="ml-4 text-xl text-bevagelse-blue-dark">
                     Kontakt
                   </div>
-                </a>
+                </NuxtLink>
               </div>
             </nav>
-          </div>
-        </div>
-        <div class="py-6 px-5">
-          <div class="mt-6">
-            <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-500 hover:bg-gray-700">
-              Book
-            </a>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        toggleBurger: false
+      }
+    },
+    watch: {
+      '$route' () {
+        this.toggleBurger = false
+      }
+    }
+  }
+</script>
